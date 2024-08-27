606a680014de590a9406c0807cce4985d4fd312a
unknown <shivamshah781@gmail.com>
2024-08-27 20:04:15 +0530

    second

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,13 +1,12 @@
 import { InitialProfile } from "../../lib/initial-profile";
 import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
+
 
 const SetupPage=async()=>{
     const profile=await InitialProfile();
-    console.log(profile)
     
     if(!profile)
-      return auth().redirectToSignIn();
+      return redirect("/sign-in");
 
     return redirect(`/feature/dashboard`);
 
